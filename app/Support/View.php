43641906ca542540d5e37968936aa95de73a8de1
sedacove <sedacove@users.noreply.github.com>
2025-11-12 17:59:21 +0300

    fixing prod

--- app/Support/View.php
+++ app/Support/View.php
@@ -20,6 +20,29 @@ class View
 
     public static function make(string $template, array $data = []): string
     {
+        // Убеждаемся, что config/config.php загружен (для функций типа asset_url)
+        static $configLoaded = false;
+        if (!$configLoaded) {
+            $configPath = __DIR__ . '/../../config/config.php';
+            if (is_file($configPath)) {
+                require_once $configPath;
+                $configLoaded = true;
+            } else {
+                // Отладка: логируем, если config не найден
+                error_log("View::make() - config.php not found at: $configPath");
+            }
+        }
+        
+        // Проверяем, что функция asset_url доступна
+        if (!function_exists('asset_url')) {
+            error_log("View::make() - asset_url() not available after loading config");
+            // Пытаемся загрузить config еще раз
+            $configPath = __DIR__ . '/../../config/config.php';
+            if (is_file($configPath)) {
+                require_once $configPath;
+            }
+        }
+
         $templatePath = self::templatePath($template);
         if (!is_file($templatePath)) {
             throw new \RuntimeException("View '{$template}' not found at {$templatePath}");
